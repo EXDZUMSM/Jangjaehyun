2e72146a847b60f4b6a15b67dd3faeca599b5503
jaehyunjang <4jh8@naver.com>
2017-06-28 18:29:20 +0900

    2017-06-28 commit

--- JJH/src/common/DBConn.java
+++ JJH/src/common/DBConn.java
@@ -1,19 +1,18 @@
 package common;
 
 public class DBConn {
-	static int a;
-	int getInt(){
+	static int a=1;
+	static int getInt(){
 		try{
 			int a = Integer.parseInt("str");
 			return a;
-			
 		}catch(Exception e){
 			e.printStackTrace();
-			return 0;	
+			return 0;
 		}
 	}
+	
 	public static void main(String[] args){
-		
 		DBConn dbc = new DBConn();
 		int a = dbc.getInt();
 	}
