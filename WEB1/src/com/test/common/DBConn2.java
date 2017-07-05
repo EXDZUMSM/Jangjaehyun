1857656933d160640498ee8d98fedbb4b10e74e5
jaehyunjang <4jh8@naver.com>
2017-07-05 18:40:14 +0900

    2017-07-05

---
+++ WEB1/src/com/test/common/DBConn2.java
@@ -0,0 +1,26 @@
+package com.test.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConn2 {
+
+	private static Connection con;
+
+	public static Connection getCon() throws ClassNotFoundException, SQLException {
+		if (con == null) {
+			Class.forName("org.mariadb.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/iot_test", "root", "Wkdwogus!2");
+			con.setAutoCommit(false);
+		}
+		return con;
+	}
+	public static void closeCon() throws SQLException {
+		if (con != null) {
+			con.close();
+			con = null;
+		}
+	}
+}
+
